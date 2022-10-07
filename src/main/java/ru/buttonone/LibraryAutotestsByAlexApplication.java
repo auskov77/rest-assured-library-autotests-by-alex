7be6375458ce8fd89077cc38060b9ab22fae8322
auskov77 <auskov77@yandex.ru>
2022-10-08 00:09:48 +0300

    master

---
+++ src/main/java/ru/buttonone/LibraryAutotestsByAlexApplication.java
@@ -0,0 +1,13 @@
+package ru.buttonone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAutotestsByAlexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryAutotestsByAlexApplication.class, args);
+	}
+
+}
